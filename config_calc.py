2023cb7e60128f982ff006b7066c034f9f2d512e
matt-long <mclong@ucar.edu>
2017-10-20 13:15:26 -0600

    moved subtree/easy-analysis to easy

--- config_calc.py
+++ config_calc.py
@@ -1,25 +1,33 @@
+#-- os interaction
 import os
 import sys
 from glob import glob
 from subprocess import call
 
-path_tools = './subtree/easy-analysis'
-sys.path.insert(0,os.path.abspath(os.path.expanduser(path_tools)))
+#-- path additions
+path_tools = ['./easy']
+for p in path_tools:
+    sys.path.insert(0,os.path.abspath(os.path.expanduser(p)))
 
+#-- analysis
 from datetime import datetime
 import xarray as xr
 import numpy as np
 
+#-- easy
 import task_manager as tm
 from regrid import regrid
 from datasrc import cesm_le
 import esm_tools as et
+easy = et.__file__
 
+#-- directories
+calc_name = 'iucn-ch'
 diro = {}
-diro['out'] = '/glade/scratch/mclong/calcs/iucn-ch'
-diro['work'] = '/glade/scratch/mclong/calcs/iucn-ch/work'
-diro['tmp'] = '/glade/scratch/mclong/tmp/iucn-ch'
-diro['fig'] =  '/glade/p/work/mclong/fig/iucn-ch'
+diro['out'] = '/glade/scratch/'+os.environ['USER']+'/calcs/'+calc_name
+diro['work'] = '/glade/scratch/'+os.environ['USER']+'/calcs/'+calc_name+'/work'
+diro['tmp'] = '/glade/scratch/'+os.environ['USER']+'/tmp'
+diro['fig'] =  '/glade/p/work/'+os.environ['USER']+'/fig/'+calc_name
 diro['logs'] = './logs'
 
 for pth in diro.values():
